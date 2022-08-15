570ba1feb17fead4a095bc023f85d01e3904cdf7
Sakyuba Noe <398noe@gmail.com>
2022-08-16 08:18:41 +0900

    Create createThread component

---
+++ src/components/CreateThread.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+export const CreateThread: React.FC = () => {
+    return (
+        <Box></Box>
+    );
+}
+
+export default CreateThread;
