98a593a8506ebad5c9b6994961e9a3b5e8094204
Sakyuba Noe <398noe@gmail.com>
2022-08-16 08:31:55 +0900

    Add routing and not found component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import './App.css';
 import Thread from "./components/Thread";
 import Layout from "./layout/Layout";
+import { Routes, Route, Link } from "react-router-dom";
+import CreateThread from "./components/CreateThread";
+import NotFound from "./components/NotFound";
 
 interface props { }
 
@@ -9,7 +12,11 @@ export const App: React.FC = (props: props) => {
 	return (
 		<div className="App">
 			<Layout>
-				<Thread />
+				<Routes>
+					<Route path="/" element={<Thread />} />
+					<Route path="/threads/new" element={<CreateThread />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
 			</Layout>
 		</div>
 	);
