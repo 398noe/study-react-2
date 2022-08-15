eb8c2144e088850090bd311b74dea985848feac8
Sakyuba Noe <398noe@gmail.com>
2022-08-16 07:47:01 +0900

    Install aspida and change commands

---
+++ aspida.config.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/** @type {Partial<import('aspida/dist/getConfigs').AspidaConfig>} */
+const aspidaConfig = {
+    input: "src/api",
+    baseURL: "https://railway-react-bulletin-board.herokuapp.com"
+}
+
+module.exports = aspidaConfig
